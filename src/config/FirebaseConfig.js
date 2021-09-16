61c032e5a97d81c5bdd2163b83f3b442037e3751
athiththan <90118973+athiththan@users.noreply.github.com>
2021-09-16 11:11:55 +0530

    Add files via upload

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC9cFdbHGnMZtOKl4KkruifW_ujqqmdy2U",
+    authDomain: "mygethub.firebaseapp.com",
+    projectId: "mygethub",
+    storageBucket: "mygethub.appspot.com",
+    messagingSenderId: "996337400029",
+    appId: "1:996337400029:web:bb5f6ca62447c6463f8334",
+    measurementId: "G-1220F87P7W"
+  };
