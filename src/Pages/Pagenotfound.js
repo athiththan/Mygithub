f6294a75ecc96f38b6d9a685cd073ba134de1ed1
athiththan <90118973+athiththan@users.noreply.github.com>
2021-09-16 11:10:46 +0530

    Add files via upload

---
+++ src/Pages/Pagenotfound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Pagenotfound = () =>{
+    return(
+        <div>
+            <hi>Pagenotfound</hi>
+        </div>
+    );
+};
+export default Pagenotfound;
---
+++ src/Pages/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Signin = () =>{
+    return(
+        <div>
+            <hi>Signin page</hi>
+        </div>
+    );
+};
+export default Signin;
